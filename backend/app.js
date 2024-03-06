377874e781782aae4289495db5e642ebd82f81d6
uttejterlapu <uterlapu@gitam.in>
2024-03-06 10:50:23 +0530

    inital commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5001;
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Heeloo")
+})
+app.get("/login",(req,res)=>{
+    res.send("login")
+})
+
+app.listen(PORT,()=>{
+    console.log("Listing to PORT " , PORT);
+})
+
+
